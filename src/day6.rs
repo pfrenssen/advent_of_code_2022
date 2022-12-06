0fe861f10217055f280af7ca146b1f765b86ce0b
Pieter Frenssen <pieter@frenssen.be>
2022-12-06 22:19:12 +0200

    Day 6.

--- src/day6.rs
+++ src/day6.rs
@@ -8,12 +8,25 @@ fn parse_input(input: &str) -> String {
 
 #[aoc(day6, part1)]
 fn part1(datastream: &str) -> usize {
-    0
+    find_non_consecutive(datastream, 4)
 }
 
 #[aoc(day6, part2)]
 fn part2(datastream: &str) -> usize {
-    0
+    find_non_consecutive(datastream, 14)
+}
+
+fn find_non_consecutive(datastream: &str, count: usize) -> usize {
+    // Loop through the datastream as a moving window of usize characters.
+    for i in 0..datastream.len() - count + 1 {
+        let window = &datastream[i..i + count];
+        // Check that there are no duplicate characters in the window.
+        if window.chars().all(|c| window.matches(c).count() == 1) {
+            return i + count;
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -31,7 +44,7 @@ mod tests {
             "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_string(),
         ];
 
-        get_test_input()
+        get_test_input_part1()
             .iter()
             .enumerate()
             .for_each(|(i, test_case)| {
@@ -42,7 +55,7 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        let test_cases = get_test_input();
+        let test_cases = get_test_input_part1();
         for (input, expected) in test_cases {
             let parsed = part1(&input);
             assert_eq!(expected, part1(&input));
@@ -51,17 +64,30 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        // let input = parse_input(get_test_input());
-        // assert_eq!("MCD", part2(&input));
+        let test_cases = get_test_input_part2();
+        for (input, expected) in test_cases {
+            let parsed = part2(&input);
+            assert_eq!(expected, part2(&input));
+        }
     }
 
-    fn get_test_input<'a>() -> Vec<(&'a str, usize)> {
+    fn get_test_input_part1<'a>() -> Vec<(&'a str, usize)> {
         vec![
             ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7),
             ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
             ("nppdvjthqldpwncqszvftbrmjlhg", 6),
             ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
             ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+        ]
+    }
+
+    fn get_test_input_part2<'a>() -> Vec<(&'a str, usize)> {
+        vec![
+            ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+            ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
         ]
     }
 }
